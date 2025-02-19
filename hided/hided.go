2eb4082ed912eb3d4b6cee27ec416e82d69e44af
Adrien Kara <adrien@iglou.eu>
2025-02-19 16:26:56 +0100

    🙈 chore(hided): add gpl license
    Signed-off-by: Adrien Kara <adrien@iglou.eu>

--- hided/hided.go
+++ hided/hided.go
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2024 Adrien Kara
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
 // Package hided provides types and methods to obfuscate or mask sensitive data
 // It can be used to ensure that sensitive information is not exposed in logs or any outputs
 package hided
